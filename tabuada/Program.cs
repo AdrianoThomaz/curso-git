9ede86bbbc379e24808dc68f791507a7b6bd0088
Adriano Thomaz <adrianomaiathomaz@gmail.com>
2019-12-28 11:26:23 -0300

    Projeto iniciado com a selecao de numero e as quatro operacoes

---
+++ tabuada/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, sinal;
+
+            Console.WriteLine("Qual a tabuada? ");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a operação? (1= Mult, 2= Div, 3= Soma, 4= Sub) ");
+            sinal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Tabuada de {x}");
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+            {
+                switch (sinal)
+                {
+                    case 1:
+                        Console.WriteLine(x + $" * {i} = " + (x * i));
+                        break;
+                    case 2:
+                        Console.WriteLine((x * i) + $" / {x} = " + i);
+                        break;
+                    case 3:
+                        Console.WriteLine(x + $" + {i} = " + (x + i));
+                        break;
+                    case 4:
+                        Console.WriteLine(x + $" - {i} = " + (x - i));
+                        break;
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
